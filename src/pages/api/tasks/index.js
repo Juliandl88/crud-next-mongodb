ad11e0a19678f5fd17d8f46935c1c62231f8fa49
Julian <julian_delisio@hotmail.com>
2022-12-09 21:23:23 -0300

    metodos

--- src/pages/api/tasks/index.js
+++ src/pages/api/tasks/index.js
@@ -1,12 +1,22 @@
-import { dbConnect } from "../../../utils/mongoose"
+import { dbConnect } from "utils/mongoose";
+import Task from "models/Task";
 
-dbConnect()
+dbConnect();
 
-export default function handler(req, res) {
-    res.status(200).json("tasks")
+export default async function handler(req, res) {
+
+  const { method, body } = req  // Desustructuro los req.method
+
+  switch (method) {
+    case "GET":
+      const tasks = await Task.find();
+      return res.status(200).json(tasks);
+
+    case "POST":
+      console.log(body)
+      return res.json("creating a task");
+
+    default:
+      return res.status(404).json({ msg: "This method is not supported" });
   }
-
- 
-
-
-  
+}
--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -4,12 +4,12 @@ const conn = {
   isConnected: false,
 };
 
-export async function dbConnect() {
-  const db = await connect(process.env.MONGODB_URI);
+export async function dbConnect() {     // Funcion para crear la conexión con Mongodb
+  const db = await connect(process.env.MONGODB_URL); // Puede ser URI
   console.log(db.connection.db.databaseName);
   conn.isConnected = db.connections[0].readyState;
 }
 
-connection.on("connected", () => console.log("Mongodb connected to db"));
+connection.on("connected", () => console.log("Mongodb connected to db")); // Evento para saber si hubo conexión
 
-connection.on("error", (err) => console.error("Mongodb Errro:", err.message));
+connection.on("error", (err) => console.error("Mongodb Errro:", err.message)); // Evento para saber si hubo un error
