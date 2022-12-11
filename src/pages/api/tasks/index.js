79f4445bd1ed6e5120b5d5e5c936e56f870c8b92
Julian <julian_delisio@hotmail.com>
2022-12-10 23:57:41 -0300

    metodos

--- src/pages/api/tasks/index.js
+++ src/pages/api/tasks/index.js
@@ -13,8 +13,9 @@ export default async function handler(req, res) {
       return res.status(200).json(tasks);
 
     case "POST":
-      console.log(body)
-      return res.json("creating a task");
+      const newTask = new Task(body) // Creamos un objeto con el título de la descripción
+      const savedTask = await newTask.save(); // Guardamos el objeto en una constante del
+      return res.status(201).json(savedTask); // Retornamos el objeto guardado
 
     default:
       return res.status(404).json({ msg: "This method is not supported" });
