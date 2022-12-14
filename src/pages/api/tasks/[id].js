cefba0e5adc50bf380f24801ca043b93fcc0fab2
Julian <julian_delisio@hotmail.com>
2022-12-13 23:50:44 -0300

    método put de la API

--- src/pages/api/tasks/[id].js
+++ src/pages/api/tasks/[id].js
@@ -20,7 +20,7 @@ export default async (req, res) => {
     case "GET": // mostrar una tarea
       try {
         const task = await Task.findById(id); // busca la tarea
-      if (!task) return res.status(404).json({ msg: "Task not found" }); // si no encuentra la tarea
+      if (!task) return res.status(404).json({ msg: "Task is not found" }); // si no encuentra la tarea
       return res.status(200).json(task);
       } catch (error) {
             return res.status(500).json({ msg: error.message }); // si no encuentra la tarea para mostrar
@@ -29,7 +29,7 @@ export default async (req, res) => {
     case "PUT": // actualizar una tarea
       try {
         const task = await Task.findByIdAndUpdate(id, body, { new : true });// busca la ID para actualizar. El parametro objeto es para que devuelva el objeto nuevo, no el viejo
-        if(!task) return res.status(404).json({ msg: "Task not found" }); // si no encuentra la tarea envia msg
+        if(!task) return res.status(404).json({ msg: "Task is not found" }); // si no encuentra la tarea envia msg
         return res.status(200).json({task}); // Si la encuentra envia un status 200
       } catch (error) {
         return res.status(500).json({ error: error.message });
@@ -37,7 +37,7 @@ export default async (req, res) => {
     case "DELETE": // borrar una tarea
       try {
            const deleteTask =  await Task.findByIdAndDelete(id);
-           if(!deleteTask) return res.status(404).json({msg: "Task not found"});
+           if(!deleteTask) return res.status(404).json({msg: "Task is not found"});
            return res.status(204).json()
       } catch (error) {
         return res.status(400).json({ msg: error.message }); // si no encuentra la tarea para borrar
