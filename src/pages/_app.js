46f11d10223da570bdbbacdc840e34f26ed3f84c
Julian <julian_delisio@hotmail.com>
2022-12-15 23:41:22 -0300

    empezando el front

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import 'semantic-ui-css/semantic.min.css' // Importamos semantic en el componente raiz
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
